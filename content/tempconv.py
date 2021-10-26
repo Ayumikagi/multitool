5d53e2dffd226d571d9a8f97b8f9308ca280b078
Ayumi <93033809+Ayumikagi@users.noreply.github.com>
2021-10-26 17:25:12 +0200

    Update tempconv.py

--- content/tempconv.py
+++ content/tempconv.py
@@ -13,21 +13,21 @@ while True:
 
 while True:
 
-    temp = input('What scale? (Celsius/Fahrenheit/Kelvin): ')
+    temp = input('What scale? (Celsius/Fahrenheit/Kelvin): ').upper()
 
-    if temp.upper() in ('CELSIUS', 'C'):
+    if temp in ('CELSIUS', 'C'):
         print(f'{num4}°C equals to {(num4*1.8)+32}F or {num4+273.15}K')
         break
 
-    elif temp.upper() in ('FAHRENHEIT', 'F'):
+    elif temp in ('FAHRENHEIT', 'F'):
         print(f'{num4}F equals to {(num4-32)*1.8}°C or {(num4-32)*1.8+273.15}K')
         break
 
-    elif temp.upper() in ('KELVIN', 'K'):
+    elif temp in ('KELVIN', 'K'):
         print(f'{num4}K equals to {num4-273.15}°C or {(num4-273.15)*1.8+32}F')
         break
 
-    elif temp.upper() == 'R':
+    elif temp == 'R':
         break
 
     else:
